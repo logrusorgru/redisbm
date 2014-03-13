1b7039a0de19225ce11b1b1a3ac9aa0f62f64b09
Konstantin Ivanov <ivanov.konstantin@logrus.org.ru>
2014-03-14 01:01:42 +0400

    small changes...

--- README.md
+++ README.md
@@ -5,32 +5,61 @@ Compare Redis HSET/HGET and SET/GET
 
 ### Require
 
-* Ruby Interpriter
-* redis ruby gem
-* hiredis ruby gem
-
-### Edit
-
-By default db connection options is
-
-```ruby
-$r = Redis.new "/tmp/redis.sock", db: 10, driver: :hiredis
-```
-
-You may to use default connection options (no need for 'hiredis' driver)
-
-```ruby
-$r = Redis.new
-```
+* [Ruby Interpriter](http://ruby-lang.org)
+* [Redis server](http://redis.io)
+* `redis` ruby gem
+* `hiredis` ruby gem ( if you want )
 
 
-### Usage
+### Installation ( Linux ) and usage
 
 ```bash
-$ ruby redisbm.rb 50000
+$ git clone https://github.com/logrusorgru/redisbm.git
+$ cd redisbm
+```
+Launch test ( see options! )
+
+```bash
+$ ruby redisbm.rb
 ```
 
-50000 - an example (10000 by default)
+### Options
+
+##### For help
+
+```bash
+$ ruby redisbm.rb --help
+```
+
+##### DB options:
+
+	--driver=default|hiredis
+		choose redis driver
+	--db-number=10
+		choose number of db, 0 by default
+	--db-socket-path ( or -s )
+		use unix domain socket instead of TCP connection ( default path is /tmp/redis.sock )
+	--prefix
+		set hashes-names and key's names prefix to protect from overwriting existing values
+	--flush
+		flush db after test ( REMOVE ALL FROM DB )
+
+##### Test options
+
+	--count
+		number of hashes ( default: 10000 )
+	--fields
+		number of each hash fields ( default: 10 )
+
+##### Exampes
+
+```bash
+ruby redisbm.rb -s -n10 --count=1000,10000,20000,40000,80000,160000 -f100
+```
+
+```bash
+ruby redisbm.rb -s -n10 -c1000,50000,100000 -f100,20,1
+```
 
 ### Legal
 
--- redisbm.rb
+++ redisbm.rb
@@ -5,62 +5,211 @@
 ###
 #
 
+# std-lib
 require 'benchmark'
+require 'optparse'
+# db
 require 'redis'
-require 'hiredis'
+# drivers
+#require 'hiredis'
+##require 'em-synchrony'
 
-$r = Redis.new path: "/tmp/redis.sock", db: 10, driver: :hiredis
+# we need the CAPTION and FORMAT constants
+include Benchmark
 
-count = ( ARGV[0].is_a? String ) ? ARGV[0].to_i : 10000
+### Parse options
 
-puts "Count #{count}"
+options = {}
 
-# denerate rnd data
+OptionParser.new do |opts|
+  opts.banner = %Q{Usage: ruby redisbm.rb [options]}
+
+  opts.on("-d", "--driver=DRIVER", "choose redis driver, --driver=default|hiredis") do |driver|
+    options[:driver] = driver
+  end
+
+  opts.on("-n", "--db-number=NUMBER", Integer, "choose redis db number (0 by default), --db-number=15") do |number|
+    options[:db_number] = number
+  end
+
+  opts.on("-s", "--db-socket-path=[PATH]", "choose redis socket path if need (\"/tmp/redis.sock\" by default)") do |path|
+  	options[:socket] = true
+    options[:path] = path
+  end
+
+  opts.on("-c", "--count=COUNT", Array, "number of hashes, 10 000 by default (may it be an Array)") do |count|
+    options[:count] = count
+  end
+
+  opts.on("-f", "--fields=FIELDS", Array, "number of hash fields, 10 by default (may it be an Array)") do |fields|
+    options[:fields] = fields
+  end
+
+  opts.on("--prefix=PREFIX", "prefix to protect from overwriting existing values") do |prefix|
+    options[:prefix] = prefix
+  end
+
+   opts.on("--flush", "flush db after test !!!REMOVE ALL DATA FROM DB!!!") do |flush|
+    options[:flush] = flush
+  end
+
+  opts.on_tail("-h", "--help", "Show this message") do
+  	puts opts
+  	puts "  Source: <http://github.com/logrusorgru/redisbm>"
+  	puts "  License: MIT"
+  	puts "  (c) 2014 Konstantin Ivanov <ivanov.konstantin@logrus.org.ru>"
+  	exit 0
+  end
+
+end.parse!
+
+###
+### Apply options 
+###
+
+redis_args = {} # db options
+
+# redis driver
+if options[:driver] == 'hiredis'
+	require 'hiredis'
+	redis_args[:driver] = options[:driver]
+end
+
+# db num
+redis_args[:db] = options[:db_number] unless options[:db_number].nil?
+
+# use socket?
+if options[:socket]
+	redis_args[:path] = options[:path].nil? ? "/tmp/redis.sock" : options[:path]
+end
+
+# more options
+
+count = options[:count].nil? ? [10_000] : options[:count]
+fields = options[:fields].nil? ? [10] : options[:fields]
+
+
+# generate random string
 
 def rnd_data
 	('a'..'z').to_a.shuffle[0..7].join
 end
 
-# generate k-v hash
+# hash name with prefix
 
-kv = {}
+$prefix = options[:prefix]
 
-count.times do |i|
-	hash_field = "#{rnd_data}-#{i}"
-	field_value = rnd_data
-	kv[i] = { hash_field => field_value }
+def hname h
+	"#{$prefix}#{h}"
 end
 
-def key k, v
-	"#{k}:#{v}"
+# hash name with prefix + field name
+
+def str_key h, f
+	"#{hname(h)}:#{f}"
 end
 
-Benchmark.bm do |b|
-	# hset/hget
-	b.report('hset/hget') do
-		# write
-		kv.each do |k, v|
-			# k - count, redis hash name
-			# v - redis hash field
-			$r.hset k, v, kv[k][v]
+### Init DB
+
+begin
+
+	$r = Redis.new redis_args
+
+	puts "Ok, let's go!"
+
+	count.each_index do |i|
+
+		puts "Step #{i+1} of #{count.size}"
+		puts "\tHashes: #{count[i]}"
+		fld = fields[i].nil? ? 10 : fields[i].to_i
+		puts "\tFields: #{fld}"
+	
+		# generate k-v hash
+	
+		kv = {}
+	
+		puts "generate values hash..."
+	
+		count[i].to_i.times do |i|
+			hash_name = hname "#{rnd_data}-#{i}"
+			fld.times do |j|
+				field_name = "#{rnd_data}-j"
+				kv[hash_name] = { field_name => rnd_data }
+			end
 		end
-		# read
-		kv.each do |k, v|
-			$r.hget k, v
+
+		puts "perfomance test..."
+
+		Benchmark.benchmark( CAPTION,
+												 16,
+												 FORMAT,
+		 										 "hset/hget total:".rjust(16),
+		 	 									 "set/get total:".rjust(16),
+		 	 		 							 "difference".rjust(16) ) do |b|
+
+			### hset/hget
+
+			# write
+			hw = b.report( 'hset write'.rjust(16) ) do
+				kv.each do |h, f|
+					# h - count, redis hash name
+					# f - redis hash field
+					$r.hset h, f, kv[h][f]
+				end
+			end
+
+			# read
+			hr = b.report( 'hget read'.rjust(16) ) do
+				kv.each do |h, f|
+					$r.hget h, f
+				end
+			end
+		
+			### set/get
+
+			# write
+			sw = b.report( 'set write'.rjust(16) ) do
+				kv.each do |h, f|
+					# h - count, redis hash name
+					# f - redis hash field
+					$r.set str_key( h, f ), kv[h][f]
+				end
+			end
+
+			# read
+			sr = b.report( 'get read'.rjust(16) ) do
+				kv.each do |h, f|
+					$r.get str_key( h, f )
+				end
+			end
+
+			# totlas and difference
+			[ hw + hr, sw + sr, hw + hr - sw - sr ]
 		end
+
+		puts "cleaning..."
+
+		kv.each do |h, f|
+			$r.del hname(h)
+			$r.del str_key( h, f )
+		end
+
+	# end of count.each
 	end
 
-	# set/get
-	b.report('set/get') do
-		# write
-		kv.each do |k, v|
-			# k - count, redis hash name
-			# v - redis hash field
-			$r.set key( k, v ), kv[k][v]
-		end
-		# read
-		kv.each do |k, v|
-			$r.get key( k, v )
-		end
-	end
+ # db connection failed
+ rescue Redis::CannotConnectError
+ 	puts "db connection failed, please check connection option ( see README for details )"
+ 	exit 0
+
 end
+
+# flush db if need
+begin
+	puts "Flush db"
+	$r.flushdb
+end if options[:flush]
+
+puts "done"
+
+exit 0
